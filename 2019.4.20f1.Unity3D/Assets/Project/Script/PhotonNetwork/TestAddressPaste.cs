5f125119987985aec9730c47c0394c103dfa7cf2
nicharleys <zxc5679120@gmail.com>
2021-02-18 01:51:34 +0800

    UI Vision Updata

---
+++ 2019.4.20f1.Unity3D/Assets/Project/Script/PhotonNetwork/TestAddressPaste.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestAddressPaste : MonoBehaviour
+{
+    [SerializeField] public InputField address;
+    private void Copy(string value) {
+        GUIUtility.systemCopyBuffer = value;
+    }
+    private string Paste() {
+        return GUIUtility.systemCopyBuffer;
+    }
+    public void CopyAddress1() {
+        Copy("0x6E427fe90d42ba92f1f426c07d362113926eb3d1");
+    }
+    public void CopyAddress2() {
+        Copy("0x65817a2bee8df38582f4124b00aa0d3ca5d87703");
+    }
+    public void CopyAddress3() {
+        Copy("0x2a25676dE8fe8378e34571be55b7B3d689EA66BF");
+    }
+    public void PasteON() {
+        address.text = Paste();
+    }
+}
